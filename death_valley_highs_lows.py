211be0b7c1ebe8203cd991c81e7ed846557a2541
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-15 23:07:37 +0300

    Hata döndüren veri satırı tespit edildi. Hata veren kod bloğu try-except-else bloğu ile elimine edilerek grafik çizdirildi.

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -12,13 +12,18 @@ with open(filename) as f:
     highs,lows,dates =[],[],[]
 
     for row in reader:
-        high=int(row[4])
-        low=int(row[4])
         current_date=datetime.strptime(row[2],'%Y-%m-%d')
+        try:
 
-        highs.append(high)
-        lows.append(low)
-        dates.append(current_date)
+            high=int(row[4])
+            low=int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+        
 
 #En yüksek sıcaklıkları ve en düşük sıcaklıkları tarihlere göre çizdir.
 plt.style.use('seaborn')
@@ -28,7 +33,8 @@ ax.plot(dates,lows,c='blue',alpha=0.5)
 plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
 
 #Grafiği formatla
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
+title="Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title,fontsize=20)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)",fontsize=16)
